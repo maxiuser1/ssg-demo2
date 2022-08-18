261f707e81b9fda3155b5f75010e9991f8ad63df
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-08-18 12:20:58 -0500

    integra supa

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import supabase from "../utils/supabase";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>hola</h1>
-    </div>
-  )
+export default function Home({posts}) {
+  return  posts.map(post =>  (
+    <p key={post.id}>
+      <Link href={`/${post.id}`}>
+        <a>{post.title}</a>
+      </Link>
+    </p>
+  ))
 }
+
+
+export const getStaticProps = async () => {
+  const { data: posts } = await supabase.from("posts").select("id, title");
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 86400,
+  };
+};
