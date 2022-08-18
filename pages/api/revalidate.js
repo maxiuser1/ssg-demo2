a876b2eb853d4809ae63fbfd020ed12890095808
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-08-18 12:24:04 -0500

    incluye revalidate y index

---
+++ pages/api/revalidate.js
@@ -0,0 +1,8 @@
+const handler = async (req, res) => {
+  await res.revalidate("/");
+  const pathToRevalidate = `/${req.body?.record?.id || req.body?.old_record?.id}`;
+  await res.revalidate(pathToRevalidate);
+  return res.send({ revalidated: true });
+};
+
+export default handler;
